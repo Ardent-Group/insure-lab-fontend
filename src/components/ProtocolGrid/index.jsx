5acb2e31f450a375eccec2fd59f319907155529a
Realkayzee <olaniyikazeem112@gmail.com>
2023-02-24 02:47:10 +0100

    protocol grid implementation

--- src/components/ProtocolGrid/index.jsx
+++ src/components/ProtocolGrid/index.jsx
@@ -2,19 +2,20 @@ import { Heading, Spacer, Stack, Flex, Text, Avatar, Image, Box, Button } from "
 import React, { lazy, FC, MouseEventHandler } from "react";
 import { useNavigate } from 'react-router-dom';
 import webLogo from '../../assets/link-2.svg'
-import secureLogo from "../../assets/shield-tick.svg"
-import AvatarLogo from '../../assets/uniswap 1.svg'
-import EthereumLogo from '../../assets/ethereum 1.svg'
+import FantomLogo from "../../assets/fantomlogo.svg"
+import SecureLogo from "../../assets/SecureDex.svg"
 import waveLogo from '../../assets/mdi_approximately-equal.png'
 
 const ProtocolGrid = ({
-  icon,
-  cardName,
-  onClick,
+  protocolName,
+  protocolLink,
+  protocolCap,
+  coverCost,
+  riskLevel,
   link,
   onOpen
 }) => {
-  const { root, text, title, subTitle, title2 } = useStyles();
+  const { root, title, subTitle, title2 } = useStyles();
 
   let navigate = useNavigate()
 
@@ -24,67 +25,60 @@ const ProtocolGrid = ({
       as="button"
       bg="#FFFFFF"
       _hover={{  boxShadow: '2xl' }}
-      onClick={onClick}
       justify="center"
-      alignItems="center"
     > 
-      <Flex>
-       <Avatar src={AvatarLogo} />
+      <Flex my={5}>
+       <Avatar src={SecureLogo} />
        <Flex flexDir="column" ml="15px" textAlign="left">
-        <Text {...title}>{cardName}</Text>
+        <Text {...title}>{protocolName}</Text>
         
         <Flex>
           <Image src={webLogo} w={"15px"} mr="5px" /> 
-          <Text {...subTitle}>www.domain.co</Text>
-         </Flex>
+          <Text {...subTitle}>{protocolLink}</Text>
+        </Flex>
 
-         <Flex>
-          <Image src={secureLogo} w={"15px"} mr="5px" /> 
-          <Text {...subTitle}>Smart contract vulnerability</Text>
-         </Flex>
        </Flex>
       
       </Flex>
 
-      <Flex justify="space-between" textAlign="left">
+      <Flex justifyContent="space-between" w={"100%"} textAlign="left">
 
         <Flex flexDir="column">
           <Text {...title2}>Chains</Text>
          <Flex mt="10px" justify="center" alignItems="center">
-          <Avatar src={EthereumLogo} width="30px" h="30px" mr="10px" />
-          <Text {...subTitle}>Ethereum</Text>
+          <Avatar src={FantomLogo} width="30px" h="30px" mr="10px" />
+          <Text {...subTitle}>Fantom</Text>
          </Flex>
         </Flex>
-          <Spacer mr="50px" />
-          <Flex flexDir="column">
+        <Flex flexDir="column">
           <Text {...title2}>Capacity</Text>
          <Flex mt="10px" justify="center" alignItems="center">
           <Avatar src={waveLogo} width="20px" h="20px" mr="10px" />
-          <Text {...subTitle}>365.4k USD</Text>
+          <Text {...subTitle}>{protocolCap} USD</Text>
          </Flex>
         </Flex>
 
       </Flex>
 
-      <Flex justify="space-between" textAlign="start" pt="24px">
+      <Flex justifyContent="space-between" w={"100%"} textAlign="start" pt="24px">
 
        <Flex flexDir="column">
-         <Text fontSize="12px" fontWeight="500">Monthly Cover cost</Text>
-        <Flex mt="10px" justify="center" alignItems="center">
-         <Text {...title}>0.22%</Text>
+         <Text fontSize="12px" fontWeight="bold">Monthly Cover cost</Text>
+        <Flex mt="10px">
+         <Text {...title}>{coverCost}</Text>
         </Flex>
        </Flex>
-         <Spacer mr="50px" />
+         {/* <Spacer mr="50px" /> */}
          <Flex flexDir="column">
          <Text {...title2}>Risk level</Text>
-        <Flex mt="5px" justify="center" alignItems="center">
-         <Text {...subTitle} color="#BA1A1A">High risk</Text>
+        <Flex mt="5px" justify="flex-end">
+         <Text {...subTitle} color="#BA1A1A">{riskLevel}</Text>
         </Flex>
        </Flex>
        </Flex>
 
        {/* ------------------------------- Button controller ---------------------------- */}
-       <Flex justify="space-between" textAlign="start" pt="24px">
+       <Flex justifyContent="space-between" textAlign="start" pt="24px" w={"100%"}>
 
         <Flex flexDir="column">
          <Button color="black" borderRadius="100px" bg="#DCE7F9"
@@ -94,11 +88,11 @@ const ProtocolGrid = ({
             Add cover
         </Button>
         </Flex>
-          <Spacer mr="60px" />
           <Flex flexDir="column">
           <Button color="white" borderRadius="100px" bg="ctaBg"
           fontSize="14px" fontWeight={500}
-          onClick={() => navigate(link)}
+          onClick={() => navigate(`/protocols/${link+1}`)}
+          px={"1.5rem"}
           >
             Insure
          </Button>
@@ -115,7 +109,6 @@ const useStyles = () => {
   return {
     root: {
       p: ["10px", "15px", "18px", "20px", "25px"],
-      h: "345px",
     },
     text: {
       fontSize: ["12px", "14px", "16px", "16px", "20px"],
