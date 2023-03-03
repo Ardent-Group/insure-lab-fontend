230a1764479197594030835058d70ea336df0cb1
Realkayzee <olaniyikazeem112@gmail.com>
2023-03-03 01:24:40 +0100

    updated constant

--- src/constants/interactionSetup.js
+++ src/constants/interactionSetup.js
@@ -7,11 +7,11 @@ import ERC20 from "../abi/ERC20Abi.json";
 
 // Deployed Contract Addresses
 
-export const mUSDT = "0x18d3BD3f2A9e1af0C2dF26a0f80E3af34abEe4F7"
+export const mUSDT = "0x86cDeb91758E251124f7CA152BfDd8a23B70d230"
 
-export const insureLabContract = "0x06bF8c3D4B8fEa715D403693d532f5bF99C47ae3"
+export const insureLabContract = "0x462C2CAC993D6F11658CD70A92B7C30398E944E6"
 
-export const governanceContract = "0xA87F30B7CD469D65B08bb74f669821259AfD0e7d"
+export const governanceContract = "0x4EC46c1c3BEaDf4621eB34B56F4b8734f115343F"
 
 
 export const insureLabSetup = {
